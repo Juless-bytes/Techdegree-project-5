bda663397fb3728f61d93b94f4628192a304d7ca
Juless-bytes <diop153@gmail.com>
2021-04-28 14:59:17 -0700

    Complete

--- js/app.js
+++ js/app.js
@@ -1,17 +1,4 @@
-  
-let input = document.getElementById('text')
+baguetteBox.run('.gallery');
 
-const handleSearch = (e) => {
-  let inputValue = e.target.value.toLowerCase();
-  let array = document.getElementsByClassName('item')
+const search = new Filter('search', 'data-caption'); 
 
-  for (let i = 0; i < array.length; i++) {
-    let image = array[i]
-    let caption = image.getAttribute('data-caption').toLowerCase()
-    let title = image.getAttribute('title').toLowerCase()
-    caption.includes(inputValue) || title.includes(inputValue)? image.style.display = 'inline' : image.style.display = 'none'
-  }
-}
-input.addEventListener('keyup', handleSearch)
-
-baguetteBox.run('.gallery'); 
